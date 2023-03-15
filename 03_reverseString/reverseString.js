28d20222ad671b60312cfa7abd5e437a345afd5c
paigebartlett <77411738+paigebartlett@users.noreply.github.com>
2023-03-14 20:44:09 -0700

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let array = inputString.split("");
+    array.reverse();
+    let outputString = ''
+    for (const character of array) {
+        outputString += character;
+    }
+    return outputString
 };
 
 // Do not edit below this line
