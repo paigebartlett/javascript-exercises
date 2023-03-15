6b369a4ff85ee81e813711b6b0041518d9b0f10f
paigebartlett <77411738+paigebartlett@users.noreply.github.com>
2023-03-14 20:22:10 -0700

    Solve repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let storeString = '';
+    if (num < 0) {
+        storeString = 'ERROR'
+    }
+    else {
+        for (i = 0; i < num; i++) {
+            storeString += string
+        }
+    }
+return storeString;
 };
 
 // Do not edit below this line
