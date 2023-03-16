d747dd9035b52e508d93da5c8c856a73a5bd16f2
paigebartlett <77411738+paigebartlett@users.noreply.github.com>
2023-03-15 20:13:40 -0700

    Solve reverse array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
 
-};
+const removeFromArray = function(array, ...numbers) {
+    for (let j = 0; j < numbers.length; j++)
+        for (let i = 0; i < array.length; i++){
+            if (array[i] === numbers[j]) {
+                array.splice(i, 1);
+            }
+        }
+    return array;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
