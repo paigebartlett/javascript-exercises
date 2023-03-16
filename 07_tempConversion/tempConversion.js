0448a64aaba85d19825dadf0cbd59b9ddb8ea3e7
paigebartlett <77411738+paigebartlett@users.noreply.github.com>
2023-03-16 07:23:21 -0700

    Solve tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsius = 
+  (farenheit - 32) * (5/9);
+  celsius = celsius.toFixed(1);
+  return +celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let farenheit = (celcius * (9/5)) + 32;
+  farenheit = farenheit.toFixed(1);
+  return +farenheit
 };
 
 // Do not edit below this line
